4c1655db8b12eca714097bd60083177ebd15cb8b
zankrogin <nathanzankich@gmail.com>
2013-07-06 00:49:30 -0700

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,64 @@
 
-
 require 'spec_helper'
 
 describe "Static pages" do
 
-  subject { page }
+subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+shared_examples_for "all static pages" do
+ it { should have_selector('h1',    text: heading) }
+ it { should have_selector('title', text: full_title(page_title)) }
+end
 
-    it { should have_selector('h1',    text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
-  end
+describe "Home page" do
+before { visit root_path }
 
-  describe "Help page" do
-    before { visit help_path }
+let(:heading)    { 'Sample App' }
+let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
-  end
+it_should_behave_like "all static pages"
+it { should_not have_selector 'title', text: '| Home' }
+end
 
-  describe "About page" do
-    before { visit about_path }
+describe "Help page" do
+before { visit help_path }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
-  end
+let(:heading)    { 'Help' }
+let(:page_title) { '' }
 
-  describe "Contact page" do
-    before { visit contact_path }
+it_should_behave_like "all static pages"  
+end
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
-  end
+describe "About page" do
+before { visit about_path }
+
+let(:heading)    { 'About Us' }
+let(:page_title) { '' }
+
+it_should_behave_like "all static pages"
+end
+
+describe "Contact page" do
+before { visit contact_path }
+
+let(:heading)    { 'Contact' }
+let(:page_title) { '' }
+
+it_should_behave_like "all static pages"
+end
+
+it "should have the right links on the layout" do
+visit root_path
+click_link "About"
+page.should have_selector 'title', text: full_title('About Us')
+click_link "Help"
+page.should have_selector 'title', text: full_title('Help')
+click_link "Contact"
+page.should have_selector 'title', text: full_title('Contact')
+click_link "Home"
+click_link "Sign up now!"
+page.should have_selector 'title', text: full_title('Sign Up')
+click_link "sample app"
+page.should_not have_selector 'title', text: full_title(' | Home')
+end
 end
